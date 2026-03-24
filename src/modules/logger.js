e38d4e5e7dfcd9bcebe798d8dc6ea260294478e6
sonicleez <sonicleez@users.noreply.github.com>
2026-03-24 15:24:19 +0700

    fix(logger): enable logs in production to debug coolify issues

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -17,13 +17,13 @@ const IS_DEV = typeof import.meta !== 'undefined' && import.meta.env?.DEV;
 const noop = () => {};
 
 export const log = {
-    debug: IS_DEV ? console.log.bind(console) : noop,
-    info: IS_DEV ? console.log.bind(console) : noop,
+    debug: console.log.bind(console),
+    info: console.log.bind(console),
     warn: console.warn.bind(console),
     error: console.error.bind(console),
-    group: IS_DEV ? console.group.bind(console) : noop,
-    groupEnd: IS_DEV ? console.groupEnd.bind(console) : noop,
-    time: IS_DEV ? console.time.bind(console) : noop,
-    timeEnd: IS_DEV ? console.timeEnd.bind(console) : noop,
-    table: IS_DEV ? console.table.bind(console) : noop,
+    group: console.group.bind(console),
+    groupEnd: console.groupEnd.bind(console),
+    time: console.time.bind(console),
+    timeEnd: console.timeEnd.bind(console),
+    table: console.table.bind(console),
 };
